cf2b08cec04e58ddc75f0eac5f6dcf323bac2023
panW <w.hodlik@wp.pl>
2024-03-01 21:26:07 +0100

    bingo card update
    25 slots of bingo card are generated by javascript. currently in's imposible to edit the content

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let editorMode = 0
+let slotList = []
 update_main_panel()
+generate_bingo()
 
 //adds event listener to the scale range (range input that controls visualization scale)
 var scaleRange = document.getElementById("vis_scale")
@@ -42,4 +44,33 @@ function update_main_panel(){
 
     menuOptions[editorMode].style.backgroundColor = 'lightGray'
     panelList[editorMode].style.display = 'flex'
+}
+
+//generates slots of bingo card within visualization
+function generate_bingo(){
+    let bingoCard = document.getElementById("visualization")
+    let slotContainer = document.getElementById("slotContainer")
+    let counter = 0; //temporary, remove later, probably
+    for(let i=0; i<5; i++){
+        //create new row
+        let newRow = document.createElement("div")
+        newRow.setAttribute("class","slotContainerRow")
+
+        for(let j=0; j<5; j++){
+            //create new element
+            let newSlot = document.createElement("div")
+            let newText = document.createTextNode("element "+counter)
+            //add class and text
+            newSlot.setAttribute("class","slot")
+            newSlot.appendChild(newText)
+
+            //add slot to the row and to the array
+            newRow.appendChild(newSlot)
+            slotList.push(newSlot)
+
+            counter++ //remove later
+        }
+        //add row to the container
+        slotContainer.appendChild(newRow)
+    }
 }
